ba9d3c7a19a1d9badfa528833d404dfaa36423ce
Ray Kivisto <ray.kivisto@gmail.com>
2018-04-26 12:08:47 -0400

    only define steps in library

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,13 +1,9 @@
 def call() {
-	stage('Build') {
-		agent any
-		steps {
-			// https://jenkins.io/blog/2016/10/16/stage-lock-milestone/
-			// The first milestone step starts tracking concurrent build order
-			milestone label: 'build', ordinal: 1
-			echo 'compiling...'
-			sh 'touch a.jar'
-			stash includes: 'a.jar', name: 'myApp'
-		}
-	}
+	// https://jenkins.io/blog/2016/10/16/stage-lock-milestone/
+	// The first milestone step starts tracking concurrent build order
+	milestone label: 'build', ordinal: 1
+	echo 'compiling...'
+	echo "boolParam=${boolParam}"
+	sh 'touch a.jar'
+	stash includes: 'a.jar', name: 'myApp'
 }
