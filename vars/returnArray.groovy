94cf60f6b1b2b45e2d5cc9b5566296b51fcc24b1
Ray Kivisto <ray.kivisto@gmail.com>
2019-05-06 11:57:24 -0400

    return an array from a shared library

---
+++ vars/returnArray.groovy
@@ -0,0 +1,17 @@
+def call(env){
+  def result = [:]
+  switch (env) {
+    case 'a':
+      result = ['a1', 'a2', 'a3']                                             
+      break
+    case 'b':
+      result = ['b1', 'b2', 'b3']                                             
+      break
+    default:
+      println "argument missing for call to returnArray(env)"
+      currentBuild.result = 'FAILURE'
+      break
+  }
+  println "Selected: " + result
+  return result
+}
