fdca3a1b5e9fbea5eb333cc785b02d23b7ba5f7e
Ray Kivisto <ray.kivisto@gmail.com>
2019-05-06 15:02:57 -0400

    test using String[]

--- vars/returnArray.groovy
+++ vars/returnArray.groovy
@@ -1,11 +1,11 @@
 def call(env){
-  def result = [:]
+  String[] result = [:]
   switch (env) {
     case 'a':
-      result = ['a1', 'a2', 'a3']                                             
+      result = ["a1", "a2", "a3"]                                             
       break
     case 'b':
-      result = ['b1', 'b2', 'b3']                                             
+      result = ["b1", "b2", "b3"]                                             
       break
     default:
       println "argument missing for call to returnArray(env)"
